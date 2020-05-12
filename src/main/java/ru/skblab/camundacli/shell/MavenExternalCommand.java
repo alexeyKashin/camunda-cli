d0feacc93dbcec2c98a46fad62962bd9fddaac93
a.kashin <al.s.kashin@gmail.com>
2020-05-12 15:47:15 +0500

    логирование

--- src/main/java/ru/skblab/camundacli/shell/MavenExternalCommand.java
+++ src/main/java/ru/skblab/camundacli/shell/MavenExternalCommand.java
@@ -1,23 +1,31 @@
 package ru.skblab.camundacli.shell;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.maven.shared.invoker.*;
 import org.springframework.boot.ansi.AnsiColor;
 import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import ru.skblab.camundacli.core.ShellHelper;
 
 import java.io.File;
 import java.util.Collections;
 
 @ShellComponent
+@Slf4j
+@AllArgsConstructor
 public class MavenExternalCommand {
+
+    private final ShellHelper shellHelper;
 
     @ShellMethod(value = "Проверка контекста среды(maven, java)", key = "ch")
     public String checkContext() {
         if (runMavenCommand("--version")) {
-            return outString("Контекст готов, проверка успешна.", AnsiColor.BRIGHT_MAGENTA);
+            return getAnsiString("Контекст готов, проверка успешна.", AnsiColor.BRIGHT_MAGENTA);
         } else {
-            return outString("Контекст не готов, проверка не успешна.", AnsiColor.BRIGHT_RED);
+            return getAnsiString("Контекст не готов, проверка не успешна.", AnsiColor.BRIGHT_RED);
         }
     }
 
@@ -29,9 +37,9 @@ public class MavenExternalCommand {
     @ShellMethod(value = "Собрать maven проект", key = "b")
     public String build() {
         if (runMavenCommand("install -Dmaven.test.skip=true")) {
-            return outString("Сборка проведена успешно.", AnsiColor.BRIGHT_MAGENTA);
+            return getAnsiString("Сборка проведена успешно.", AnsiColor.BRIGHT_MAGENTA);
         } else {
-            return outString("Сборку провести не удалось!", AnsiColor.BRIGHT_RED);
+            return getAnsiString("Сборку провести не удалось!", AnsiColor.BRIGHT_RED);
         }
     }
 
@@ -40,7 +48,7 @@ public class MavenExternalCommand {
         return "";
     }
 
-    private String outString(String outString, AnsiColor color) {
+    private String getAnsiString(String outString, AnsiColor color) {
         if (color == null) {
             color = AnsiColor.BRIGHT_MAGENTA;
         }
@@ -51,19 +59,23 @@ public class MavenExternalCommand {
     private Boolean runMavenCommand(String command) {
         Invoker invoker = new DefaultInvoker();
         String mavenHome = System.getenv().get("MAVEN_HOME");
-        invoker.setMavenHome(new File(mavenHome));
+        if (mavenHome == null) {
+            shellHelper.printError("Не установлена переменная среды MAVEN_HOME");
+            return false;
+        }
 
+        invoker.setMavenHome(new File(mavenHome));
         InvocationRequest request = new DefaultInvocationRequest();
         request.setGoals(Collections.singletonList(command));
 
         try {
             invoker.execute(request);
-            return true;
         } catch (MavenInvocationException e) {
-            e.printStackTrace();
+            shellHelper.printError("Ошибка при выполнении команды " + command);
+            log.error("{}", e.getMessage());
+            log.error("{}", ExceptionUtils.getStackTrace(e));
             return false;
         }
-
+        return true;
     }
-
 }
